33b32ac49a874d93d9e4e72edb1f67973473da6c
marko.silla@ericsson.com <marko.silla@ericsson.com>
2016-12-31 02:23:12 +0200

    Added nUnit project Zoo.Test

---
+++ Loomaaed/Zoo.Test/ZooDALUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Zoo.Test
+{
+    [TestFixture]
+    public class ZooDALNUnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
